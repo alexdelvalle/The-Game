e9259e9de18dd1eefc488e532c126ae985f9b012
Alex Del Valle <amd407@gmail.com>
2017-11-01 15:58:14 -0400

    Added bean array movement functionality

--- js/coffee.js
+++ js/coffee.js
@@ -54,9 +54,12 @@ function gamePageInit(){
       }
   };
 
+  // Coffee Mug Bullet
+
+
   // Coffee Projectiles
   var beanImage = new Image();
-  beanImage.src = './images/beans.png';
+  beanImage.src = './images/bean1.png';
 
   function Beans (x, y, caffeinePts, isConsumed, image, isLoaded) {
       this.x = x;
@@ -69,16 +72,58 @@ function gamePageInit(){
 
   Beans.prototype.draw = function () {
       // if (this.isLoaded) {
-          ctx.drawImage(this.image, this.x, this.y, 10, 10);
+          ctx.drawImage(this.image, this.x, this.y, 15, 20);
       // }
   };
 
-  var myBeans = [
-      new Beans (400, 100, 5, false, beanImage, false),
-      new Beans (415, 100, 5, false, beanImage, false),
-      new Beans (430, 100, 5, false, beanImage, false),
-      new Beans (445, 100, 5, false, beanImage, false),
+  var myBeansOne = [];
+      for(i = 1; i <= 12; i++) {
+          myBeansOne.push(new Beans (400 + (50 * i), 120, 5, false, beanImage, false, 0.7));
+  }
+
+  var myBeansTwo = [];
+      for(i = 1; i <= 12; i++) {
+          myBeansTwo.push(new Beans (400 + (50 * i), 165, 5, false, beanImage, false));
+  }
+
+  var myBeansThree = [];
+      for(i = 1; i <= 12; i++) {
+          myBeansThree.push(new Beans (400 + (50 * i), 210, 5, false, beanImage, false));
+  }
+  var myBeansFour = [];
+      for(i = 1; i <= 12; i++) {
+          myBeansFour.push(new Beans (400 + (50 * i), 255, 5, false, beanImage, false));
+  }
+
+  var myBeansFive = [];
+      for(i = 1; i <= 12; i++) {
+          myBeansFive.push(new Beans (400 + (50 * i), 300, 5, false, beanImage, false));
+  }
+
+  var myBeansSix = [];
+      for(i = 1; i <= 12; i++) {
+          myBeansSix.push(new Beans (400 + (50 * i), 345, 5, false, beanImage, false));
+  }
+
+  var allOddBeans = [
+      myBeansOne,
+      myBeansThree,
+      myBeansFive,
   ];
+
+  var allEvenBeans = [
+      myBeansTwo,
+      myBeansFour,
+      myBeansSix,
+  ];
+
+  // must all be separate to not mix up the positive and negative values for the array draw functions below
+  var ax = 0.8;
+  var bx = 0.8;
+  var cx = 0.8;
+  var dx = 0.8;
+  var ex = 0.8;
+  var fx = 0.8;
 
 
   // Draw Items
@@ -89,9 +134,76 @@ function gamePageInit(){
     myCoffeeHouse.draw();
     myGirl.draw();
 
-    myBeans.forEach(function (oneBean) {
+    myBeansOne.forEach(function(oneBean) {
         oneBean.draw();
-        oneBean.x += 0.8;
+
+        if (oneBean.x < 430) {
+            ax = -ax;
+        }
+        if (oneBean.x > canvas.width - 230) {
+            ax = -ax;
+        }
+        oneBean.x -= ax;
+    });
+
+    myBeansTwo.forEach(function(oneBean) {
+        oneBean.draw();
+
+        if (oneBean.x > canvas.width - 230) {
+            bx = -bx;
+        }
+        if (oneBean.x < 430) {
+            bx = -bx;
+        }
+        oneBean.x += bx;
+    });
+
+    myBeansThree.forEach(function(oneBean) {
+        oneBean.draw();
+
+        if (oneBean.x < 430) {
+            cx = -cx;
+        }
+        if (oneBean.x > canvas.width - 230) {
+            cx = -cx;
+        }
+        oneBean.x -= cx;
+    });
+
+    myBeansFour.forEach(function(oneBean) {
+        oneBean.draw();
+
+        if (oneBean.x > canvas.width - 230) {
+            dx = -dx;
+        }
+        if (oneBean.x < 430) {
+            dx = -dx;
+        }
+        oneBean.x += dx;
+    });
+
+    myBeansFive.forEach(function(oneBean) {
+        oneBean.draw();
+
+        if (oneBean.x < 430) {
+            ex = -ex;
+        }
+        if (oneBean.x > canvas.width - 230) {
+            ex = -ex;
+        }
+        oneBean.x -= ex;
+    });
+
+    myBeansSix.forEach(function(oneBean) {
+        oneBean.draw();
+
+        if (oneBean.x > canvas.width - 230) {
+            fx = -fx;
+        }
+        if (oneBean.x < 430) {
+            fx = -fx;
+        }
+        oneBean.x += fx;
     });
 
     requestAnimationFrame(draw);
@@ -111,7 +223,11 @@ function gamePageInit(){
             case 39: // right arrow
                 myGirl.x += 30;
                 break;
+
+            // case 32: // spacebar
+            //     mugBullet.y += 30;
+            //     break;
           } // switch
       }); // keydown
   }); // document ready
-}
+} // game initialization
